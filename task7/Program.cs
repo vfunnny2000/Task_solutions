f14071b5da3dbfa9245352e1746466ebff55e8f7
AlekseySvi <104575517+AlekseySvi@users.noreply.github.com>
2022-08-05 13:57:25 +0300

    Add files via upload

---
+++ task7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N > 99 & N < 1000)
+{
+    Console.WriteLine(N%10);
+}
+else
+{
+    Console.WriteLine("Число не подходит");
+}

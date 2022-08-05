0b49b3e959881c342479b39e79bb83d0d4495bfa
AlekseySvi <104575517+AlekseySvi@users.noreply.github.com>
2022-08-05 13:59:53 +0300

    Add files via upload

---
+++ task5/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+//-----------Для неотрицательного числа--------------------
+/*while(count <= N)
+{
+    Console.WriteLine(count);
+    count++;
+}
+*/
+//-----------Для любого целого числа-----------------------
+if(count < N)
+{
+    while(count <= N)
+    {
+        Console.WriteLine(count);
+        count++;
+    }
+}
+else
+{
+    while(count >= N)
+    {
+        Console.WriteLine(N);
+        N++;
+    }
+}
+

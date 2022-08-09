39042d6b16b38879b29f4d97418c33513a816ec4
AlekseySvi <104575517+AlekseySvi@users.noreply.github.com>
2022-08-09 17:04:52 +0300

    Add files via upload

---
+++ task14/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ task16/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA*numberA == numberB || numberB * numberB == numberA)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}

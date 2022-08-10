129f04af7c1b5bc830ff7119ce48771b4a1b74e8
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-10 20:15:45 +0300

    added 12

--- task12/Program.cs
+++ task12/Program.cs
@@ -4,7 +4,7 @@
 16, 4 -> кратно.
  */
 
-/* Console.WriteLine("Введите число А: ");
+Console.WriteLine("Введите число А: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
@@ -16,7 +16,8 @@ else
 {
     Console.WriteLine($"Не кратно, остаток {numberA % numberB}");
 }
- */
+ 
+ ---------------------------*/
 
 
 Console.WriteLine("Введите число А: ");
